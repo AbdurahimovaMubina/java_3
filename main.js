d0dbfc6126aaeb45d776789d576591effcb9433e
AbdurahimovaMubina <mubinaabdurahimova87@gmail.com>
2024-09-19 15:13:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// let num = +prompt('Lyuboy son kiriting: ')
+
+// if (num > 0) {
+//     console.log(num+1);
+// }else if (num == 0) {
+//     console.log("Bu nol soni, bu son na manfiy(-) na musbat(+) son");
+// }else {
+//     console.log(num);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let person = {
+//     firstName: 'Bahodir',
+//     lastName: 'Abduqahhorov',
+//     age: 21,
+// }
+
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.age);
+
+
+let ism = prompt("Ismingizni kiriting: ")
+let id = prompt("Space ID kiriting: ")
+let password = +prompt("Parol kiriting: ")
+
+let person = {
+    firstName: ism,
+    id: id,
+    password: password
+}
+
+console.log(person);
